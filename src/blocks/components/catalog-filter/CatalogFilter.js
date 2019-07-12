cd18867f90c8824dddf8e141cb3e1501900c8707
Dina Hrist <hdina@inbox.ru>
2019-07-12 22:01:38 +0700

    Done catalog sidebar

--- src/blocks/components/catalog-filter/CatalogFilter.js
+++ src/blocks/components/catalog-filter/CatalogFilter.js
@@ -1,13 +1,24 @@
 import $ from "jquery";
 
 export class CatalogFilter {
-	constructor(selector = '.js-catalog-filter') {
+	constructor(selector = '.js-catalog-filter', dropdown = '.js-dropdown') {
 		this.filter = $(selector);
+
+		this.dropdown = dropdown;
+		this.dropdownList = $(dropdown);
 
 		this.init();
 	}
 	init() {
+		this.onClickDropdown();
+	}
 
+	onClickDropdown() {
+		const self = this;
+		this.dropdownList.find(`${this.dropdown}-name`).on('click', function () {
+			$(this).siblings(`${self.dropdown}-block`).slideToggle();
+			$(this).parents(self.dropdown).toggleClass('active');
+		});
 	}
 
 }
